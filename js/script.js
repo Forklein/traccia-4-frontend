3d4ba8e0f11f82fab6b587f4d9ee7b9dda3e0d06
Forklein <giuseppepisani2@gmail.com>
2022-05-16 22:02:49 +0200

    Bugfix

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-console.log('ok');
+const text = 'Smashing the UX';
+const element = document.querySelector('#wrapper');
 
-// Quesito: Al candida	to si chiede di animare la frase "Smashing the UX" mediante gsap.
-// Ogni lettera deve entrare in fadein con uno scaleup da 0 a 1.
-// L'animazione di ogni lettera deve partire poco prima del termine della precedente realizzando un 
-// effetto a cascata.
-// Opzionale
-// Gradita la costruzione di una meccanica acquistize di una frase custom fornita dall'utente.
+const animateText = (text) => {
+    const splitText = text.split('');
+    let i = 0;
+    const runAnimate = setInterval(() => {
+        if(i === (splitText.length -1)){
+            clearInterval(runAnimate);
+        }
+        const textnode = document.createElement(`div-${i}`);
+        if(splitText[i] === " ") {
+            splitText[i] = "&nbsp;";     
+        }
+        element.append(textnode);
+        textnode.innerHTML = splitText[i];
+        console.log(textnode);
+        TweenMax.fromTo(textnode, 1+i, {scale: 0}, {scale: 1}, 0);
+        i++;
+    }, 500);
+}
 
 
-// const text = 'Smashing the UX';
-// const element = document.querySelector('#wrapper');
+animateText(text);
+
 
 
 // const animateText = (text) => {
@@ -21,18 +34,20 @@ console.log('ok');
 //     })
 // }
 
+// animateText(text);
 
-var textDiv = $("#textDiv"),
-    sentence = textDiv.html().split(""),
-    tl = new TimelineMax({repeat:10, repeatDelay:0.4, yoyo:true}); 
+
+// var textDiv = $("#textDiv"),
+//     sentence = textDiv.html().split(""),
+//     tl = new TimelineMax({repeat:10, repeatDelay:0.4, yoyo:true}); 
     
-TweenLite.set(textDiv, {css:{perspective:500, perspectiveOrigin:"50% 50%", transformStyle:"preserve-3d"}});
-textDiv.html("");
+// TweenLite.set(textDiv, {css:{perspective:500, perspectiveOrigin:"50% 50%", transformStyle:"preserve-3d"}});
+// textDiv.html("");
 
-$.each(sentence, function(index, val) {
-    if(val === " ") {
-        val = "&nbsp;";     
-    }
-    var letter = $("<span/>", {id : "txt" + index}).html(val).appendTo(textDiv);
-    tl.to(letter, 1, {autoAlpha:1, ease:Back.easeOut,  rotationX:360, transformOrigin:"50% 50% -20"}, index * 0.05);
-});
+// $.each(sentence, function(index, val) {
+//     if(val === " ") {
+//         val = "&nbsp;";     
+//     }
+//     var letter = $("<span/>", {id : "txt" + index}).html(val).appendTo(textDiv);
+//     tl.to(letter, 1, {autoAlpha:1, ease:Back.easeOut,  rotationX:360, transformOrigin:"50% 50% -20"}, index * 0.05);
+// });
