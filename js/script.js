4ff7796ee7e04e5dd9afddeedbeef3bfd897ba48
Giuseppe <giuseppe.pisani@webora.it>
2022-05-16 18:44:17 +0200

    Add jquery and change logic

--- js/script.js
+++ js/script.js
@@ -8,5 +8,31 @@ console.log('ok');
 // Gradita la costruzione di una meccanica acquistize di una frase custom fornita dall'utente.
 
 
-gsap.registerPlugin(SplitText);
-TweenMax.fromTo('#wrapper', 2, {scale: 0}, {scale: 1}, 0);
+// const text = 'Smashing the UX';
+// const element = document.querySelector('#wrapper');
+
+
+// const animateText = (text) => {
+//     const splitText = text.split('');
+//     splitText.forEach((character)=> {
+//         const textnode = document.createTextNode(character);
+//         element.append(textnode);
+//         TweenMax.fromTo('#wrapper', 1, {scale: 0}, {scale: 1}, 0);
+//     })
+// }
+
+
+var textDiv = $("#textDiv"),
+    sentence = textDiv.html().split(""),
+    tl = new TimelineMax({repeat:10, repeatDelay:0.4, yoyo:true}); 
+    
+TweenLite.set(textDiv, {css:{perspective:500, perspectiveOrigin:"50% 50%", transformStyle:"preserve-3d"}});
+textDiv.html("");
+
+$.each(sentence, function(index, val) {
+    if(val === " ") {
+        val = "&nbsp;";     
+    }
+    var letter = $("<span/>", {id : "txt" + index}).html(val).appendTo(textDiv);
+    tl.to(letter, 1, {autoAlpha:1, ease:Back.easeOut,  rotationX:360, transformOrigin:"50% 50% -20"}, index * 0.05);
+});
